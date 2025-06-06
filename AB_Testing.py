01d5b5ddf9be5b4a6e531355c2f2f221cb2e2053
Janith <janithpe@gmail.com>
2025-06-06 13:42:58 -0700

    Added Z-test

--- AB_Testing.py
+++ AB_Testing.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from statsmodels.stats.proportion import proportions_ztest
 
 # Reading the data from Data folder and understanding
 ab_test = pd.read_csv("Data/ab_test.csv")
@@ -25,8 +26,16 @@ print(f"Duplicated user_id found: {ab_test["user_id"].duplicated().any()}")
 crosstabDF = pd.crosstab(ab_test.group, ab_test.landing_page, margins=True)
 print(crosstabDF)
 
+# Crosstab plot
 crosstabDF.plot.bar(figsize=(10,5))
+plt.title("Distribution of Landing Pages by Group")
+plt.xlabel("Experiment Group")
+plt.ylabel("Number of Users")
+plt.legend(title="Landing Page")
+plt.xticks(rotation=0)
 plt.grid(axis="y")
+plt.tight_layout()
+plt.show()
 
 mismatch_mask = (
     ((ab_test["group"] == "treatment") & (ab_test["landing_page"] == "old_page")) |
@@ -50,3 +59,21 @@ print()
 print(ab_test_cleaned["group"].value_counts())
 print()
 print(ab_test_cleaned.groupby("group")["converted"].mean())
+print()
+
+# Group-level conversion stats
+conversion_sum = ab_test_cleaned.groupby("group")["converted"].agg(["sum", "count"])
+print(conversion_sum)
+
+converted = conversion_sum["sum"].values
+n_obs = conversion_sum["count"].values
+
+# Performing two-proportion Z-test
+z_score, p_value = proportions_ztest(count=converted, nobs=n_obs, alternative='two-sided')
+
+# Results
+print("Conversion rates:")
+print(conversion_sum["sum"]/conversion_sum["count"])
+print()
+print("Z-score:", round(z_score, 4))
+print("P-value:", round(p_value, 4))
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EcommerceABTesting
 Analyzing an A/B Test for an E-commerce Checkout Redesign
+
+## 1. Objective
+Evaluate whther a redesigned checkout page improved conversion rates compared to the existing page.
+
+
